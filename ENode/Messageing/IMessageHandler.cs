d79fe7b74ea5218d225404b16880a480e19ee7b3
huahuajiang <33571988+huahuajiang@users.noreply.github.com>
2020-12-29 22:54:14 +0800

    2020/12/29 third

---
+++ ENode/Messageing/IMessageHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ENode.Messageing
+{
+    public interface IMessageHandler
+    {
+    }
+
+    public interface IMessageHandler<in T>:IMessageHandler where T : class, IMessage
+    {
+
+    }
+}
---
+++ NoteSample.QuickStart/EventHandlers/NoteEventHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoteSample.QuickStart.EventHandlers
+{
+    public class NoteEventHandler:IMessageHandler
+    {
+    }
+}
