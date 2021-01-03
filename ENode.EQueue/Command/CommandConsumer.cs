07b196d10b3fa2185719a1be7d26cca5940ebe7d
huahuajiang <33571988+huahuajiang@users.noreply.github.com>
2021-01-03 21:54:52 +0800

    2021/1/3

--- ENode.EQueue/Command/CommandConsumer.cs
+++ ENode.EQueue/Command/CommandConsumer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ECommon.Serializing;
+using ENode.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using IQueueMessageHandler = EQueue.Clients.Consumers.IMessageHandler;
@@ -8,6 +10,9 @@ namespace ENode.EQueue.Command
     public class CommandConsumer: IQueueMessageHandler
     {
         private const string DefaultCommandConsumerGroup = "CommandConsumerGroup";
-        private SendReplyService
+        private SendReplyService _sendRepllyService;
+        private IJsonSerializer _jsonSerializer;
+        private ITypeNameProvider _typeNameProvider;
+        private ICommandProcessor
     }
 }
---
+++ ENode.EQueue/Command/ICommandProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ENode.EQueue.Command
+{
+    public interface ICommandProcessor
+    {
+        void Process(ProcessingCommand)
+    }
+}
